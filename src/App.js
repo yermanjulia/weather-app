0c16555b4ed771b4a1de6b46c5876e06ceab8dd0
Julia Yerman <juliyaerm88@gmail.com>
2023-06-16 11:07:39 -0700

    Created conteiners and added API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=d3ae555664e37a418e1f35755557a860`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container"> </div>
+      <div className="top">
+        <div className="location">
+          <p>Dallas</p>
+        </div>
+        <div className="temp">
+          {" "}
+          <h1>65 °F</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p>65 °F </p>
+        </div>
+        <div className="humidity">
+          <p> 20%</p>
+        </div>
+        <div className="wind">12 MPH</div>
+      </div>
     </div>
   );
 }
