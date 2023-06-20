39acf19593358d9eb4d92a760de5d21bf5d2d45a
Julia Yerman <juliyaerm88@gmail.com>
2023-06-20 13:27:45 -0700

    Updated index.css file

--- src/App.js
+++ src/App.js
@@ -6,27 +6,32 @@ function App() {
 
   return (
     <div className="app">
-      <div className="container"> </div>
-      <div className="top">
-        <div className="location">
-          <p>Dallas</p>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>65 °F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
         </div>
-        <div className="temp">
-          {" "}
-          <h1>65 °F</h1>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65 °F </p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold"> 20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold"> 12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
         </div>
-        <div className="description">
-          <p>Clouds</p>
-        </div>
-      </div>
-      <div className="bottom">
-        <div className="feels">
-          <p>65 °F </p>
-        </div>
-        <div className="humidity">
-          <p> 20%</p>
-        </div>
-        <div className="wind">12 MPH</div>
       </div>
     </div>
   );
