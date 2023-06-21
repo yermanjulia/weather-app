bda0dbd24ca6bbf9fd18d2e00338e5c51085480e
Julia Yerman <juliyaerm88@gmail.com>
2023-06-21 11:21:38 -0700

    Code formatted

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
           </div>
         </div>
 
-        {data.name != undefined && (
+        {data.name !== undefined && (
           <div className="bottom">
             <div className="feels">
               {data.main ? (
